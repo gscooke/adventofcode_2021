2bb7cffcb9b21684affc6be33816bc7a6ffa1f6b
gscooke <george@personacode.co.uk>
2021-12-02 13:39:46 +0000

    vscode elements

--- puzzles/BasePuzzle.cs
+++ puzzles/BasePuzzle.cs
@@ -1,6 +1,6 @@
 internal class BasePuzzle {
     internal void StartPuzzleOutput(string puzzleName) {
-        Console.WriteLine($"{Environment.NewLine}Starting {nameof(Day2)}");
+        Console.WriteLine($"{Environment.NewLine}Starting {puzzleName}");
     }
 
     internal List<string> LoadInputs(string fileName) {
